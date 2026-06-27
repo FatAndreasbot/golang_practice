e8283fedc14bde5df53b58fc6508e2c9a5e1b0f5
Andrew <guva.andrej.01@gmail.com>
2026-06-27 18:45:25 +0700

    homework WIP

---
+++ practice/25_06/code.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	url := "https://www.wildberries.ru/getPurchasesByID?id=%d"
+	responses := make([]int, 100_000, 100_000)
+	jobs := make(chan int, 100_000)
+	var working sync.WaitGroup
+
+	for worker := range 300 {
+		go func() {
+			for id := range jobs {
+				responses[id] = getPurchasesByID(id, url)
+				working.Done()
+			}
+		}()
+	}
+
+	for jobID := range 100_000 {
+		jobs <- jobID
+		working.Add()
+	}
+
+	working.Wait()
+	jobs.close()
+
+	for _, repsonse := range responses {
+		fmt.Println(response)
+	}
+}
+
+// -------------------------------------------------------
+
+type Chat struct {
+	messages []string
+	lock     sync.RWMutex
+}
+
+func NewChat() Chat {
+	return Chat{
+		messages: make([]string, 0),
+		lock:     sync.RWMutex{},
+	}
+}
+
+func (c *Chat) Receive(id int) (string, error) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	exists := id < len(c.messages)
+	if !exists {
+		return "", fmt.Errorf("message with id %d does not exists", id)
+	}
+	msg := c.messages[id]
+	return msg, nil
+}
+
+func (c *Chat) Send(msg string) int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.messages = append(c.messages, msg)
+	return len(c.messages) - 1
+}
